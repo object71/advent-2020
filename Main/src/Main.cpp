794b8127f1f44b3d02129cd408433ade869b18d7
Hristo Stamenov <capitane71@gmail.com>
2020-12-01 17:12:30 +0200

    Added first line of code.

--- Main/src/Main.cpp
+++ Main/src/Main.cpp
@@ -1,30 +1,8 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples in Notepad++, provided with default raylib installer package,
-*   just press F6 and run [raylib_compile_execute] script, it will compile and execute.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on [C:\raylib\raylib\examples] directory and
-*   raylib official webpage: [www.raylib.com]
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "Main.h"
 
 int main()
 {
-    
+    std::cout << fmt::format("Advent 2020 Programming") << std::endl;
     
     
     return 0;
--- Main/src/Main.h
+++ Main/src/Main.h
@@ -5,4 +5,7 @@
 #ifndef RAYLIBDOOM_MAIN_H
 #define RAYLIBDOOM_MAIN_H
 
+#include "fmt/format.h"
+#include <iostream>
+
 #endif //RAYLIBDOOM_MAIN_H
